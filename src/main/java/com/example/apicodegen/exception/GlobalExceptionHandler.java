ec0e19e13fb635796620cd91de0ba907b698866e
Asutosh Pandya <ashu936@gmail.com>
2026-05-31 21:31:36 -0500

    Updated error handling

--- src/main/java/com/example/apicodegen/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/apicodegen/exception/GlobalExceptionHandler.java
@@ -8,11 +8,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleNoResourceFound(NoResourceFoundException ex) {
+        // Don't log missing static resources (favicon.ico, etc.) as errors
+        log.debug("Resource not found: {}", ex.getResourcePath());
+        return "error/404";
+    }
 
     @ExceptionHandler(SpecValidationException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
